0f6628e3e57b5609fd87e13a7e2c20f905c3123e
Jorge Valdivia <valdi.jorge@gmail.com>
2018-05-14 10:45:03 +0930

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import applyRules from "./applyRules";
+import rulesRunner from "./rulesRunner";
 import { validateFields } from "./actions/validateAction";
 import { findRelSchemaAndField, findRelUiSchema } from "./utils";
 
 export { validateFields, findRelSchemaAndField, findRelUiSchema };
-
+export rulesRunner;
 export default applyRules;
